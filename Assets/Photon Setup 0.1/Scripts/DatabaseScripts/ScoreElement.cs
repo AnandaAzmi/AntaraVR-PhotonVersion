70e50ad1f2e9664a7fa94c3f834099ba6aa6119b
Ananda Syafakhatul Azmi <anandaazmi2907@gmail.com>
2025-05-06 11:54:37 +0700

    Revert "revert this"
    This reverts commit aca5c354a6032d99954f3302dd61e59321c4e157.

--- Assets/Photon Setup 0.1/Scripts/DatabaseScripts/ScoreElement.cs
+++ Assets/Photon Setup 0.1/Scripts/DatabaseScripts/ScoreElement.cs
@@ -1,4 +1,5 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -6,11 +7,14 @@ public class ScoreElement : MonoBehaviour
 {
 
     public TMP_Text usernameText;
+    //public TMP_Text killsText;
+    //public TMP_Text deathsText;
     public TMP_Text scoreText;
 
     public void NewScoreElement(string _username,  int _score)
     {
         usernameText.text = _username;
+     
         scoreText.text = _score.ToString();
     }
 
